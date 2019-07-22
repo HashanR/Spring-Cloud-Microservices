2e297a86eba01c009578373c22d5805e9ad385a3
HashanR <rmhashan@gmail.com>
2019-07-22 23:58:34 +0530

    Add feign dependency

--- Currency-Conversion-Service/src/main/java/com/hashanr/currencyconversion/CurrencyConversionServiceApplication.java
+++ Currency-Conversion-Service/src/main/java/com/hashanr/currencyconversion/CurrencyConversionServiceApplication.java
@@ -2,8 +2,12 @@ package com.hashanr.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients("com.hashanr.currencyconversion")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
